e685569ed5176c51136fe1bbf6d204e2f2da36e4
Mohammad Fadhil Nouhar A P <mohammad.fadhil@vonnue.com>
2026-07-16 15:17:14 +0530

    chore: scaffold​ ​SPA architecture

---
+++ Day-5/js/utils.js
@@ -0,0 +1,13 @@
+export function register(path, component) {}
+
+export function navigate(path) {}
+
+export function createButton() {}
+
+export function createCard() {}
+
+export function createModal() {}
+
+export function fetchJSON(url) {}
+
+export function createStore() {}
