5572fc533ad308a4bb24af7dd02c68138c113931
Mohammad Fadhil Nouhar A P <mohammad.fadhil@vonnue.com>
2026-07-17 10:42:01 +0530

    feat: Build router and state management

--- Day-5/js/pages/detail.js
+++ Day-5/js/pages/detail.js
@@ -1,1 +1,11 @@
-export function renderDetailPage() {}
+export function renderDetailPage() {
+    const documentFragment = document.createDocumentFragment();
+    const divElement = document.createElement("div");
+    const h2Element = document.createElement("h2");
+    h2Element.textContent = "Detail Page";
+    divElement.append(h2Element);
+    documentFragment.append(divElement);
+    const mainElement = document.querySelector("main");
+    mainElement.innerHTML = "";
+    mainElement.append(documentFragment);
+}
